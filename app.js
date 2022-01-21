3d1edfff5505d0d7625af52632eb1b0a18fc929c
Dzhekill <dzhek31@gmail.com>
2022-01-22 00:50:22 +0200

    try to commit

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const addButton = document.querySelector(".todo-add-button");
 const text = document.querySelector(".todo-text");
 const template = document.querySelector(".template").content;
+const listItem = document.querySelector('.todo-list-item');
+const newTemplate = template.querySelector('.todo-list-item');
 const list = document.querySelector(".todo-list");
 const items = list.children;
-const checkbox = document.querySelector('.todo-list-input');
-console.log(template);
 
+
+
+
+let addChange = function(item) {
+    const checkbox = item.querySelector(".todo-list-input");
+    checkbox.addEventListener("change", function (evt) {
+        //  evt.target.parentNode.parentNode.remove();
+        console.log(evt.target.querySelector('.todo-list-item'));
+        item.remove();
+      });
+}
+
+addChange(listItem);
 addButton.addEventListener("click", function () {
-    let task = template.cloneNode(true);
-    let taskText = task.querySelector(".todo-list-text");
-    taskText.textContent = text.value;
-    list.appendChild(task);
-    
+  let task = newTemplate.cloneNode(true);
+  let taskText = task.querySelector(".todo-list-text");
+  taskText.textContent = text.value;
+  addChange(task);
+  list.appendChild(task);
 });
-checkbox.addEventListener("change",  function(evt) {
-    evt.target.parentNode.remove();
-})
